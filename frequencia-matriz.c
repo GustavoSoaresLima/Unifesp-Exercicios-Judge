4670d762c1e2b37a214b0f8e313143dab2140aee
Gustavo Silva Soares de Lima <gustavosilvasoareslima@gmail.com>
2022-07-16 15:48:27 -0300

    Update frequencia-matriz.c

--- frequencia-matriz.c
+++ frequencia-matriz.c
@@ -2,65 +2,25 @@
 
 int main(){
 
-  int menor, linhas, colunas, matriz[30][30], matrizSaida[30][30], contador[30][30], valores[101], lista[102], frequencia[101], listaFrequencia[101], quantidade = 0;
+  int linhas, colunas, matriz[30][30], valores[101];
 
   scanf("%d%d", &linhas, &colunas);
+
+  for(int i = 0; i < 101; i++){
+    valores[i] = 0;
+  }
 
   for(int i = 0; i < linhas; i++){
     for(int j = 0; j < colunas; j++){
       scanf("%d", &matriz[i][j]);
-      matrizSaida[i][j] = matriz[i][j];
+      valores[matriz[i][j]]++;
     }
   }
 
-  for(int i = 0; i < linhas; i++){
-    for(int j = 0; j < colunas; j++){
-      contador[i][j] = 1;
-      for(int y = i; y < linhas; y++){
-        if(y == i){
-          for(int z = j + 1; z < colunas; z++){
-            if(matriz[i][j] == matrizSaida[y][z]){
-              contador[i][j]++;
-              matrizSaida[y][z] = -1;
-            }
-          }
-        } else{
-          for(int z = 0; z < colunas; z++){
-            if(matriz[i][j] == matrizSaida[y][z]){
-              contador[i][j]++;
-              matrizSaida[y][z] = -1;
-            }
-          }
-        }
-      }
+  for(int i = 0; i < 101; i++){
+    if(valores[i]){
+      printf("%d %d\n", i, valores[i]);
     }
-  }
-
-  for(int i = 0; i < linhas; i++){
-    for(int j = 0; j < colunas; j++){
-      if(matrizSaida[i][j] > -1){
-        valores[quantidade] = matrizSaida[i][j];
-        frequencia[quantidade] = contador[i][j];
-        quantidade ++;
-      }
-    }
-  }
-
-  lista[-1] = -1;
-
-  for(int i = 0; i < quantidade; i++){
-    menor = 101;
-    for(int j = 0; j < quantidade; j++){
-      if(valores[j] < menor && valores[j] > lista[i-1]){
-        menor = valores[j];
-        listaFrequencia[i] = frequencia[j];
-      }
-    }
-    lista[i] = menor;
-  }
-
-  for(int i = 0; i < quantidade; i++){
-    printf("%d %d\n", lista[i], listaFrequencia[i]);
   }
 
   return 0;
